a2df961512cb8595c41f23465531ada136810f69
NickMisael <40251538+NickMisael@users.noreply.github.com>
2023-07-20 23:06:37 -0300

    Create convertCase.c

---
+++ convertCase.c
@@ -0,0 +1,32 @@
+#include <stdio.h>
+
+ char convertCase(unsigned char ch) {
+    unsigned char result;
+
+    asm(
+        "xorb $0x20, %%al;"
+        "movb %%al, %0;"
+
+        : "=r" (result)
+        : "r" (&ch)
+        : "%eax"
+    );
+
+    return result;
+}
+
+
+int main(){
+
+    unsigned char str[100] = "HELLO world";
+
+    printf("String Original: %s\n", str);
+
+    printf("String Alterada: ");
+    for(int i = 0; i < 11; i++){
+        putchar(convertCase(str[i]));
+    }
+    putchar(0x0a);
+
+    return 0;
+}
