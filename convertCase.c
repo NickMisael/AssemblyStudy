bb8b9129d122b46573adcbce4945d8cf601cb6a7
NickMisael <40251538+NickMisael@users.noreply.github.com>
2023-07-22 04:27:34 -0300

    Update convertCase.c

--- convertCase.c
+++ convertCase.c
@@ -3,7 +3,6 @@
  char convertCase(unsigned char ch) {
     unsigned char result;
 
-   // Assembly InLine Codeblocks' Sintax
     asm(
         "xorb $0x20, %%al;"
         "movb %%al, %0;"
@@ -16,16 +15,39 @@
     return result;
 }
 
+int MyStrLen(char * str) {
+    int count = 0;
+
+    for(; *str != '\0'; str++)
+        count++;
+
+    return count;
+}
 
 int main(){
 
-    unsigned char str[100] = "HELLO world";
+    unsigned char str[100];
+
+    printf("[!!] Warning [!!]\n"
+           "There are a limit of 100 Bytes!\n");
+
+    printf("Type a string: ");
+    fgets(str, sizeof(char)*99, stdin);
+
+    if(str[MyStrLen(str)-1] == 0x0a)
+        str[MyStrLen(str)-1] = 0x0;
+    //scanf("%s", str);
 
     printf("String Original: %s\n", str);
 
+    int sizeStr = MyStrLen(str);
+
     printf("String Alterada: ");
-    for(int i = 0; i < 11; i++){
-        putchar(convertCase(str[i]));
+    for(int i = 0; i < sizeStr; i++){
+        if( (str[i] > 0x40 && str[i] < 0x5b) || (str[i] > 0x60 && str[i] < 0x7b))
+            putchar(convertCase(str[i]));
+        else
+            putchar(str[i]);
     }
     putchar(0x0a);
 
